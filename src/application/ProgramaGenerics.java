67bc938a9cf7f582c07908312c84e5e522b19c25
Rodrigo Wanzeler <wanzervai@gmail.com>
2021-11-19 11:03:26 -0300

    With String

--- src/application/ProgramaGenerics.java
+++ src/application/ProgramaGenerics.java
@@ -10,18 +10,18 @@ public class ProgramaGenerics {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		PrintService<Integer>  ps = new PrintService<>();
+		PrintService<String>  ps = new PrintService<>();
 		
 		System.out.print("How many values? ");
 		int n = sc.nextInt();
 		
 		for(int i = 0; i < n; i++) {
-			Integer value = sc.nextInt();
+			String value = sc.next();
 			ps.addValue(value);
 		}
 		
 		ps.print();
-		Integer x = ps.first();
+		String x = ps.first();
 	    System.out.println("First: " + x);
 		
 		sc.close();
